32541b703f0c99988f89122320ef0a968723187a
PresentKim <qjrmckd@gmail.com>
2018-07-03 20:07:09 +0900

    Reformat code

--- src/kim/present/dustbin/DustBin.php
+++ src/kim/present/dustbin/DustBin.php
@@ -4,96 +4,94 @@ declare(strict_types=1);
 
 namespace kim\present\dustbin;
 
-use pocketmine\Player;
-use pocketmine\command\{
-  Command, PluginCommand, CommandExecutor, CommandSender
-};
-use pocketmine\plugin\PluginBase;
 use kim\present\dustbin\inventory\DustBinInventory;
 use kim\present\dustbin\lang\PluginLang;
+use pocketmine\command\{
+	Command, CommandExecutor, CommandSender, PluginCommand
+};
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
 
 class DustBin extends PluginBase implements CommandExecutor{
+	/**
+	 * @var DustBin
+	 */
+	private static $instance = null;
 
-    /** @var DustBin */
-    private static $instance = null;
+	/**
+	 * @return DustBin
+	 */
+	public static function getInstance() : DustBin{
+		return self::$instance;
+	}
 
-    /** @return DustBin */
-    public static function getInstance() : DustBin{
-        return self::$instance;
-    }
+	/**
+	 * @var PluginLang
+	 */
+	private $language;
 
-    /** @var PluginCommand */
-    private $command = null;
+	/**
+	 * @var PluginCommand
+	 */
+	private $command;
 
-    /** @var PluginLang */
-    private $language;
+	public function onLoad() : void{
+		self::$instance = $this;
+	}
 
-    public function onLoad() : void{
-        self::$instance = $this;
-    }
+	public function onEnable() : void{
+		$dataFolder = $this->getDataFolder();
+		if(!file_exists($dataFolder)){
+			mkdir($dataFolder, 0777, true);
+		}
+		$this->language = new PluginLang($this);
 
-    public function onEnable() : void{
-        $dataFolder = $this->getDataFolder();
-        if (!file_exists($dataFolder)) {
-            mkdir($dataFolder, 0777, true);
-        }
-        $this->language = new PluginLang($this);
+		if($this->command !== null){
+			$this->getServer()->getCommandMap()->unregister($this->command);
+		}
+		$this->command = new PluginCommand($this->language->translate('commands.dustbin'), $this);
+		$this->command->setPermission('dustbin.cmd');
+		$this->command->setDescription($this->language->translate('commands.dustbin.description'));
+		$this->command->setUsage($this->language->translate('commands.dustbin.usage'));
+		if(is_array($aliases = $this->language->getArray('commands.dustbin.aliases'))){
+			$this->command->setAliases($aliases);
+		}
+		$this->getServer()->getCommandMap()->register('dustbin', $this->command);
+	}
 
-        if ($this->command !== null) {
-            $this->getServer()->getCommandMap()->unregister($this->command);
-        }
-        $this->command = new PluginCommand($this->language->translate('commands.dustbin'), $this);
-        $this->command->setPermission('dustbin.cmd');
-        $this->command->setDescription($this->language->translate('commands.dustbin.description'));
-        $this->command->setUsage($this->language->translate('commands.dustbin.usage'));
-        if (is_array($aliases = $this->language->getArray('commands.dustbin.aliases'))) {
-            $this->command->setAliases($aliases);
-        }
-        $this->getServer()->getCommandMap()->register('dustbin', $this->command);
-    }
+	/**
+	 * @param CommandSender $sender
+	 * @param Command       $command
+	 * @param string        $label
+	 * @param string[]      $args
+	 *
+	 * @return bool
+	 */
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if($sender instanceof Player){
+			$sender->addWindow(new DustBinInventory());
+		}else{
+			$sender->sendMessage($this->language->translate('commands.generic.onlyPlayer'));
+		}
+		return true;
+	}
 
-    /**
-     * @param CommandSender $sender
-     * @param Command       $command
-     * @param string        $label
-     * @param string[]      $args
-     *
-     * @return bool
-     */
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if ($sender instanceof Player) {
-            $sender->addWindow(new DustBinInventory());
-        } else {
-            $sender->sendMessage($this->language->translate('commands.generic.onlyPlayer'));
-        }
-        return true;
-    }
+	/**
+	 * @return PluginLang
+	 */
+	public function getLanguage() : PluginLang{
+		return $this->language;
+	}
 
-    /**
-     * @param string $name = ''
-     *
-     * @return PluginCommand
-     */
-    public function getCommand(string $name = '') : PluginCommand{
-        return $this->command;
-    }
-
-    /**
-     * @return PluginLang
-     */
-    public function getLanguage() : PluginLang{
-        return $this->language;
-    }
-
-    /**
-     * @return string
-     */
-    public function getSourceFolder() : string{
-        $pharPath = \Phar::running();
-        if (empty($pharPath)) {
-            return dirname(__FILE__, 4) . DIRECTORY_SEPARATOR;
-        } else {
-            return $pharPath . DIRECTORY_SEPARATOR;
-        }
-    }
+	/**
+	 * @return string
+	 */
+	public function getSourceFolder() : string{
+		$pharPath = \Phar::running();
+		if(empty($pharPath)){
+			return dirname(__FILE__, 4) . DIRECTORY_SEPARATOR;
+		}else{
+			return $pharPath . DIRECTORY_SEPARATOR;
+		}
+	}
 }
--- src/kim/present/dustbin/inventory/DustBinInventory.php
+++ src/kim/present/dustbin/inventory/DustBinInventory.php
@@ -4,118 +4,125 @@ declare(strict_types=1);
 
 namespace kim\present\dustbin\inventory;
 
+use kim\present\dustbin\DustBin;
 use pocketmine\block\{
-  Block, BlockFactory
+	Block, BlockFactory
+};
+use pocketmine\inventory\{
+	BaseInventory, CustomInventory
 };
 use pocketmine\math\Vector3;
-use pocketmine\Player;
-use pocketmine\inventory\{
-  BaseInventory, CustomInventory
-};
 use pocketmine\nbt\NetworkLittleEndianNBTStream;
 use pocketmine\nbt\tag\{
-  CompoundTag, StringTag
+	CompoundTag, StringTag
 };
 use pocketmine\network\mcpe\protocol\{
-  UpdateBlockPacket, BlockEntityDataPacket, ContainerOpenPacket
+	BlockEntityDataPacket, ContainerOpenPacket, UpdateBlockPacket
 };
 use pocketmine\network\mcpe\protocol\types\WindowTypes;
+use pocketmine\Player;
 use pocketmine\tile\Spawnable;
-use kim\present\dustbin\DustBin;
 
 class DustBinInventory extends CustomInventory{
+	/**
+	 * @var CompoundTag
+	 */
+	private $nbt;
 
-    /** CompoundTag */
-    private $nbt;
+	public function __construct(){
+		parent::__construct(new Vector3(), [], $this->getDefaultSize(), null);
 
-    public function __construct(){
-        parent::__construct(new Vector3(), [], $this->getDefaultSize(), null);
+		$this->nbt = new CompoundTag('', [
+			new StringTag('id', 'Chest'),
+			new StringTag('CustomName', DustBin::getInstance()->getLanguage()->translate('dustbin.name')),
+		]);
+	}
 
-        $this->nbt = new CompoundTag('', [
-          new StringTag('id', 'Chest'),
-          new StringTag('CustomName', DustBin::getInstance()->getLanguage()->translate('dustbin.name')),
-        ]);
-    }
+	/**
+	 * @param Player $who
+	 */
+	public function onOpen(Player $who) : void{
+		BaseInventory::onOpen($who);
 
-    /**
-     * @param Player $who
-     */
-    public function onOpen(Player $who) : void{
-        BaseInventory::onOpen($who);
+		$this->holder = $who->subtract(0, 3, 0)->floor();
+		if($this->holder->y < 0){
+			$this->holder->y = 0;
+		}
 
-        $this->holder = $who->subtract(0, 3, 0)->floor();
-        if ($this->holder->y < 0) {
-            $this->holder->y = 0;
-        }
-
-        $pk = new UpdateBlockPacket();
-        $pk->blockRuntimeId = BlockFactory::toStaticRuntimeId(Block::CHEST);
-        $pk->x = $this->holder->x;
-        $pk->y = $this->holder->y;
-        $pk->z = $this->holder->z;
-        $pk->flags = UpdateBlockPacket::FLAG_NONE;
-        $who->sendDataPacket($pk);
-
-
-        $this->nbt->setInt('x', $this->holder->x);
-        $this->nbt->setInt('y', $this->holder->y);
-        $this->nbt->setInt('z', $this->holder->z);
-
-        $pk = new BlockEntityDataPacket();
-        $pk->x = $this->holder->x;
-        $pk->y = $this->holder->y;
-        $pk->z = $this->holder->z;
-        $pk->namedtag = (new NetworkLittleEndianNBTStream())->write($this->nbt);
-        $who->sendDataPacket($pk);
+		$pk = new UpdateBlockPacket();
+		$pk->blockRuntimeId = BlockFactory::toStaticRuntimeId(Block::CHEST);
+		$pk->x = $this->holder->x;
+		$pk->y = $this->holder->y;
+		$pk->z = $this->holder->z;
+		$pk->flags = UpdateBlockPacket::FLAG_NONE;
+		$who->sendDataPacket($pk);
 
 
-        $pk = new ContainerOpenPacket();
-        $pk->type = WindowTypes::CONTAINER;
-        $pk->entityUniqueId = -1;
-        $pk->x = $this->holder->x;
-        $pk->y = $this->holder->y;
-        $pk->z = $this->holder->z;
-        $pk->windowId = $who->getWindowId($this);
-        $who->sendDataPacket($pk);
+		$this->nbt->setInt('x', $this->holder->x);
+		$this->nbt->setInt('y', $this->holder->y);
+		$this->nbt->setInt('z', $this->holder->z);
 
-        $this->sendContents($who);
-    }
+		$pk = new BlockEntityDataPacket();
+		$pk->x = $this->holder->x;
+		$pk->y = $this->holder->y;
+		$pk->z = $this->holder->z;
+		$pk->namedtag = (new NetworkLittleEndianNBTStream())->write($this->nbt);
+		$who->sendDataPacket($pk);
 
-    /**
-     * @param Player $who
-     */
-    public function onClose(Player $who) : void{
-        BaseInventory::onClose($who);
 
-        $block = $who->getLevel()->getBlock($this->holder);
+		$pk = new ContainerOpenPacket();
+		$pk->type = WindowTypes::CONTAINER;
+		$pk->entityUniqueId = -1;
+		$pk->x = $this->holder->x;
+		$pk->y = $this->holder->y;
+		$pk->z = $this->holder->z;
+		$pk->windowId = $who->getWindowId($this);
+		$who->sendDataPacket($pk);
 
-        $pk = new UpdateBlockPacket();
-        $pk->x = $this->holder->x;
-        $pk->y = $this->holder->y;
-        $pk->z = $this->holder->z;
-        $pk->blockRuntimeId = BlockFactory::toStaticRuntimeId($block->getId(), $block->getDamage());
-        $pk->flags = UpdateBlockPacket::FLAG_NONE;
-        $who->sendDataPacket($pk);
+		$this->sendContents($who);
+	}
 
-        $tile = $who->getLevel()->getTile($this->holder);
-        if ($tile instanceof Spawnable) {
-            $who->sendDataPacket($tile->createSpawnPacket());
-        }
-        $this->clearAll();
-    }
+	/**
+	 * @param Player $who
+	 */
+	public function onClose(Player $who) : void{
+		BaseInventory::onClose($who);
 
-    /** @return string */
-    public function getName() : string{
-        return "DustBin";
-    }
+		$block = $who->getLevel()->getBlock($this->holder);
 
-    /** @return int */
-    public function getDefaultSize() : int{
-        return 27;
-    }
+		$pk = new UpdateBlockPacket();
+		$pk->x = $this->holder->x;
+		$pk->y = $this->holder->y;
+		$pk->z = $this->holder->z;
+		$pk->blockRuntimeId = BlockFactory::toStaticRuntimeId($block->getId(), $block->getDamage());
+		$pk->flags = UpdateBlockPacket::FLAG_NONE;
+		$who->sendDataPacket($pk);
 
-    /** @return int */
-    public function getNetworkType() : int{
-        return WindowTypes::CONTAINER;
-    }
+		$tile = $who->getLevel()->getTile($this->holder);
+		if($tile instanceof Spawnable){
+			$who->sendDataPacket($tile->createSpawnPacket());
+		}
+		$this->clearAll();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() : string{
+		return "DustBin";
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getDefaultSize() : int{
+		return 27;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getNetworkType() : int{
+		return WindowTypes::CONTAINER;
+	}
 }
